a0cfd4951e73aac711b0eca82293a3dacabc304d
vinamra <vinamra.mattoo@porteamedical.com>
2018-01-12 01:29:10 +0530

    added bootstrap template
    Signed-off-by: vinamra <vinamra.mattoo@porteamedical.com>

--- src/main/java/com/vinamra/expenseManager/expenseManager/ExpenseManagerApplication.java
+++ src/main/java/com/vinamra/expenseManager/expenseManager/ExpenseManagerApplication.java
@@ -2,6 +2,11 @@ package com.vinamra.expenseManager.expenseManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 public class ExpenseManagerApplication {
@@ -10,4 +15,19 @@ public class ExpenseManagerApplication {
 		SpringApplication.run(ExpenseManagerApplication.class, args);
 	}
 
+
+	@Bean
+	public ViewResolver viewResolver() {
+		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+		templateResolver.setTemplateMode("XHTML");
+		templateResolver.setPrefix("views/");
+		templateResolver.setSuffix(".html");
+
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setTemplateResolver(templateResolver);
+
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(engine);
+		return viewResolver;
+	}
 }
--- src/main/java/com/vinamra/expenseManager/expenseManager/domain/WebController.java
+++ src/main/java/com/vinamra/expenseManager/expenseManager/domain/WebController.java
@@ -9,14 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
 
-    // inject via application.properties
-    @Value("${welcome.message:1234}")
-    private String message = "Hello World";
 
     @RequestMapping("/")
-    public String welcome(Map<String, Object> model) {
-        model.put("message", this.message);
-        return "welcome";
+    public String welcome() {
+        return "index";
     }
 
 }
