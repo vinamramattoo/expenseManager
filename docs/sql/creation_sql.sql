34f5ff841780ddcd9e044591accfc6ceb6a74d96
vinamra <vinamra.mattoo@porteamedical.com>
2018-01-12 23:07:52 +0530

    refactored sql
    Signed-off-by: vinamra <vinamra.mattoo@porteamedical.com>

--- docs/sql/creation_sql.sql
+++ docs/sql/creation_sql.sql
@@ -1,4 +1,19 @@
+create database expense;
+use expense;
 CREATE TABLE `expenses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` int(11) NOT NULL,
+  `amount` decimal(15,2) NOT NULL,
+  `details` VARCHAR(256) NOT NULL,
+  `created_on` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` int(11) NOT NULL,
   `amount` decimal(15,2) NOT NULL,
@@ -19,15 +34,3 @@ CREATE TABLE `expense_category_mapping` (
   FOREIGN KEY (`category_id`) REFERENCES category(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` int(11) NOT NULL,
-  `amount` decimal(15,2) NOT NULL,
-  `details` VARCHAR(256) NOT NULL,,
-  `created_on` datetime NOT NULL,
-  `created_by` int(11) NOT NULL,
-  `updated_on` datetime NOT NULL,
-  `updated_by` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
